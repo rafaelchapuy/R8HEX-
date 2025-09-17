c73926b8b64996df9f6f611583e6977b09d6685d
rafaelchapuy <rafael.chapuy.pro@gmail.com>
2025-09-17 15:51:42 +0200

    data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "8497197295:AAGga-rgqEneCHWTIgI6WUiGKj6tgGavihQ",
   id:"6744065880",
-  address:"Enter your url"
+  address:"https://5a145231-03ee-43a6-bad4-65421bfacc49-00-3mvhagipci3h6.worf.replit.dev"
 };
